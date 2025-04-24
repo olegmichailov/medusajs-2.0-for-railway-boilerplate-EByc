fe09f5f89922a4167580ad9e9db7a241427b3761
olegmichailov <olegmikhailov698@gmail.com>
2025-04-24 23:08:27 +0200

    Update medusa-config.js

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -47,17 +47,17 @@ const medusaConfig = {
       key: Modules.FILE,
       resolve: '@medusajs/file',
       options: {
+        upload_url: "https://pub-3ddc657c9b4f4fbab865c0d434eacd33.r2.dev/medusa-media/", // ✅ Важно
         providers: [
           {
             resolve: '@medusajs/file-s3',
             id: 'r2',
             options: {
-              endpoint: 'https://<ACCOUNT_ID>.r2.cloudflarestorage.com',
+              endpoint: 'https://pub-3ddc657c9b4f4fbab865c0d434eacd33.r2.dev',
               accessKeyId: CLOUDFLARE_R2_ACCESS_KEY,
               secretAccessKey: CLOUDFLARE_R2_SECRET_KEY,
               bucket: 'gmorklstrage',
-              region: 'auto',
-              s3ForcePathStyle: false
+              s3ForcePathStyle: true
             }
           }
         ]
