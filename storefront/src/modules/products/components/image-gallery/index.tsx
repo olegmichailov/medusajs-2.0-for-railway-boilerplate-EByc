a254f83248be0953b8d6ebc9d4cd68e25ef3d8d7
olegmichailov <olegmikhailov698@gmail.com>
2025-04-27 14:37:40 +0200

    Update index.tsx

--- storefront/src/modules/products/components/image-gallery/index.tsx
+++ storefront/src/modules/products/components/image-gallery/index.tsx
@@ -1,68 +1,26 @@
-// src/modules/products/components/image-gallery/index.tsx
+"use client"
 
-'use client'
-
-import { useKeenSlider } from "keen-slider/react"
-import "keen-slider/keen-slider.min.css"
 import { HttpTypes } from "@medusajs/types"
 import { Container } from "@medusajs/ui"
 import Image from "next/image"
-import { useState, useEffect } from "react"
-import { useWindowSize } from "react-use"
 
-interface ImageGalleryProps {
+type ImageGalleryProps = {
   images: HttpTypes.StoreProductImage[]
 }
 
 const ImageGallery = ({ images }: ImageGalleryProps) => {
-  const { width } = useWindowSize()
-
-  const isMobile = width < 768
-
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-    loop: false,
-  })
-
-  if (!images || images.length === 0) {
-    return null
-  }
-
   return (
-    <div className="flex flex-col items-center w-full">
-      {isMobile ? (
-        <>
-          <div ref={sliderRef} className="keen-slider w-full">
-            {images.map((image, i) => (
-              <div
-                key={image.id}
-                className="keen-slider__slide aspect-[29/34] relative bg-ui-bg-subtle"
-              >
-                <Image
-                  src={image.url}
-                  alt={`Product image ${i + 1}`}
-                  fill
-                  className="object-cover"
-                  sizes="(max-width: 768px) 100vw, 50vw"
-                />
-              </div>
-            ))}
-          </div>
-          <div className="flex justify-center gap-2 mt-4">
-            {images.map((_, i) => (
-              <div key={i} className="w-2 h-2 bg-gray-400 rounded-full opacity-50"></div>
-            ))}
-          </div>
-        </>
-      ) : (
-        <div className="flex flex-col gap-y-4">
-          {images.map((image, index) => {
-            const isPriority = index === 0
-            return (
-              <Container
-                key={image.id}
-                className="relative aspect-[29/34] w-full overflow-hidden bg-ui-bg-subtle"
-                id={image.id}
-              >
+    <div className="flex items-start relative">
+      <div className="flex flex-col flex-1 small:mx-16 gap-y-4">
+        {images.map((image, index) => {
+          const isPriority = index === 0
+          return (
+            <Container
+              key={image.id}
+              className="relative aspect-[29/34] w-full overflow-hidden bg-ui-bg-subtle"
+              id={image.id}
+            >
+              {!!image.url && (
                 <Image
                   src={image.url}
                   alt={`Product image ${index + 1}`}
@@ -73,11 +31,11 @@ const ImageGallery = ({ images }: ImageGalleryProps) => {
                   style={{ objectFit: "cover" }}
                   unoptimized={false}
                 />
-              </Container>
-            )
-          })}
-        </div>
-      )}
+              )}
+            </Container>
+          )
+        })}
+      </div>
     </div>
   )
 }
