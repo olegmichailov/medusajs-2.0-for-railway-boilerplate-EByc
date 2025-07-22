d2d06f5f78b9f5986c1ededeeaaa605c36029e6b
olegmichailov <olegmikhailov698@gmail.com>
2025-07-22 14:03:59 +0200

    Update index.tsx

--- storefront/src/modules/store/templates/index.tsx
+++ storefront/src/modules/store/templates/index.tsx
@@ -1,51 +1,50 @@
-"use client"
-
 import { Suspense } from "react"
-import { notFound } from "next/navigation"
 
-import InteractiveLink from "@modules/common/components/interactive-link"
 import SkeletonProductGrid from "@modules/skeletons/templates/skeleton-product-grid"
-import RefinementList from "@modules/store/components/refinement-list"
-import PaginatedProducts from "@modules/store/templates/paginated-products"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
-import { HttpTypes } from "@medusajs/types"
+import RefinementSidebar from "@modules/store/components/refinement-list/refinement-sidebar"
+import PaginatedProducts from "./paginated-products"
 
-export default function StoreTemplate({
-  products,
-  region,
-  countryCode,
-  sortBy,
-  page,
-}: {
-  products?: HttpTypes.StoreProduct[]
-  region: HttpTypes.StoreRegion
-  countryCode: string
+type Props = {
   sortBy?: SortOptions
   page?: string
-}) {
+  countryCode: string
+  categoryId?: string
+  collectionId?: string
+}
+
+const StoreTemplate = ({
+  sortBy,
+  page,
+  countryCode,
+  categoryId,
+  collectionId,
+}: Props) => {
   const pageNumber = page ? parseInt(page) : 1
   const sort = sortBy || "created_at"
 
-  if (!region || !countryCode) notFound()
-
   return (
-    <div
-      className="flex flex-col small:flex-row small:items-start py-6 content-container"
-      data-testid="store-container"
-    >
-      <RefinementList sortBy={sort} data-testid="sort-by-container" />
+    <div className="flex flex-col small:flex-row small:items-start py-6 content-container">
+      <div className="w-full small:max-w-[200px] mb-8">
+        <RefinementSidebar sortBy={sort} />
+      </div>
+
       <div className="w-full">
-        <div className="flex flex-row mb-8 text-2xl-semi gap-4">
-          <h1 data-testid="store-page-title">All Products</h1>
+        <div className="mb-8 text-2xl-semi">
+          <h1>All products</h1>
         </div>
         <Suspense fallback={<SkeletonProductGrid />}>
           <PaginatedProducts
             sortBy={sort}
             page={pageNumber}
             countryCode={countryCode}
+            categoryId={categoryId}
+            collectionId={collectionId}
           />
         </Suspense>
       </div>
     </div>
   )
 }
+
+export default StoreTemplate
