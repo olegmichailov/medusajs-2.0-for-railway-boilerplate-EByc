aabc2c1fe8ef073ba7e159ed66cd17f0d07214dc
olegmichailov <olegmikhailov698@gmail.com>
2025-07-24 23:02:32 +0200

    Update index.tsx

--- storefront/src/modules/skeletons/templates/skeleton-product-grid/index.tsx
+++ storefront/src/modules/skeletons/templates/skeleton-product-grid/index.tsx
@@ -1,13 +1,28 @@
 import repeat from "@lib/util/repeat"
 import SkeletonProductPreview from "@modules/skeletons/components/skeleton-product-preview"
 
-const SkeletonProductGrid = () => {
+type Props = {
+  columns?: number
+}
+
+const SkeletonProductGrid = ({ columns = 2 }: Props) => {
+  const totalItems = columns * 4 // 4 строки
+
+  const gridColsClass =
+    columns === 1
+      ? "grid-cols-1"
+      : columns === 2
+      ? "grid-cols-2"
+      : columns === 3
+      ? "grid-cols-3"
+      : "grid-cols-4"
+
   return (
     <ul
-      className="grid grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-10 px-6 sm:px-0"
+      className={`grid ${gridColsClass} gap-x-4 gap-y-10 px-6 sm:px-0`}
       data-testid="products-list-loader"
     >
-      {repeat(8).map((index) => (
+      {repeat(totalItems).map((index) => (
         <li key={index}>
           <SkeletonProductPreview />
         </li>
