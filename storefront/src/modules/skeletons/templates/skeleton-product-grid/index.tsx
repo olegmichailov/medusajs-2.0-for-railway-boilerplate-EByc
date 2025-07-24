a83aafaa185b54d343433deff9e7cd5cabba2ce9
olegmichailov <olegmikhailov698@gmail.com>
2025-07-24 18:14:37 +0200

    Update index.tsx

--- storefront/src/modules/skeletons/templates/skeleton-product-grid/index.tsx
+++ storefront/src/modules/skeletons/templates/skeleton-product-grid/index.tsx
@@ -4,7 +4,7 @@ import SkeletonProductPreview from "@modules/skeletons/components/skeleton-produ
 const SkeletonProductGrid = () => {
   return (
     <ul
-      className="grid grid-cols-1 small:grid-cols-2 medium:grid-cols-3 large:grid-cols-4 gap-x-4 gap-y-10 px-6 sm:px-0"
+      className="grid grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-10 px-6 sm:px-0"
       data-testid="products-list-loader"
     >
       {repeat(8).map((index) => (
