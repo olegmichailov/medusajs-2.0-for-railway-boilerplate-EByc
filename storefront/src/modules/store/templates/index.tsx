e11a078db1de0fb6117f87c28c2fd5f9e9f3b385
olegmichailov <olegmikhailov698@gmail.com>
2025-07-24 19:57:10 +0200

    Update index.tsx

--- storefront/src/modules/store/templates/index.tsx
+++ storefront/src/modules/store/templates/index.tsx
@@ -1,5 +1,6 @@
-'use client'
+import { Suspense } from "react"
 
+import SkeletonProductGrid from "@modules/skeletons/templates/skeleton-product-grid"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
 import RefinementSidebar from "@modules/store/components/refinement-list/refinement-sidebar"
 import PaginatedProducts from "./paginated-products"
@@ -32,13 +33,15 @@ const StoreTemplate = ({
         <div className="mb-8 text-2xl-semi">
           <h1>All products</h1>
         </div>
-        <PaginatedProducts
-          sortBy={sort}
-          page={pageNumber}
-          countryCode={countryCode}
-          categoryId={categoryId}
-          collectionId={collectionId}
-        />
+        <Suspense fallback={<SkeletonProductGrid />}>
+          <PaginatedProducts
+            sortBy={sort}
+            page={pageNumber}
+            countryCode={countryCode}
+            categoryId={categoryId}
+            collectionId={collectionId}
+          />
+        </Suspense>
       </div>
     </div>
   )
