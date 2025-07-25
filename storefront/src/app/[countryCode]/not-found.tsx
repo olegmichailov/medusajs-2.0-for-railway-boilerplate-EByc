de4fa09e2d2cc193fa99bee5db694ddbd18ada2a
olegmichailov <olegmikhailov698@gmail.com>
2025-07-25 19:54:06 +0200

    Create not-found.tsx

---
+++ storefront/src/app/[countryCode]/not-found.tsx
@@ -0,0 +1,8 @@
+// src/app/[countryCode]/not-found.tsx
+export default function NotFound() {
+  return (
+    <div style={{ minHeight: "60vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <h1 style={{ fontSize: 48, fontWeight: 500 }}>404 – Not Found</h1>
+    </div>
+  );
+}
