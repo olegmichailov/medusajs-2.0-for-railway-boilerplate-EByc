07518dd49052b2dbdec5937033bf3761276c59f9
olegmichailov <olegmikhailov698@gmail.com>
2025-07-31 16:18:18 +0200

    Update stripe.ts

--- storefront/src/lib/stripe.ts
+++ storefront/src/lib/stripe.ts
@@ -1,6 +1,7 @@
 import Stripe from "stripe"
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-04-10", // актуальная версия Stripe
-  typescript: true,
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2022-11-15",
 })
+
+export default stripe
