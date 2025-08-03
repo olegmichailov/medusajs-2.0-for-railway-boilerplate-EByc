5fa49252e4b75d75bd5262e65010af49a911570d
olegmichailov <olegmikhailov698@gmail.com>
2025-08-03 21:30:21 +0200

    Update page.tsx

--- storefront/src/app/[countryCode]/(main)/store/page.tsx
+++ storefront/src/app/[countryCode]/(main)/store/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from "next"
-
-import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
 import StoreTemplate from "@modules/store/templates"
+import { getProductsListWithSort } from "@lib/data/products"
+import { getRegion } from "@lib/data/regions"
+import { HttpTypes } from "@medusajs/types"
 
 export const metadata: Metadata = {
   title: "Store",
@@ -9,27 +10,39 @@ export const metadata: Metadata = {
 }
 
 type Params = {
-  searchParams: {
-    sortBy?: SortOptions
-    page?: string
-    categoryId?: string
-    collectionId?: string
-  }
   params: {
     countryCode: string
   }
+  searchParams: {
+    sortBy?: "created_at" | "price_asc" | "price_desc"
+  }
 }
 
-export default async function StorePage({ searchParams, params }: Params) {
-  const { sortBy, page, categoryId, collectionId } = searchParams
+export default async function StorePage({ params, searchParams }: Params) {
+  const countryCode = params.countryCode
+  const sortBy = searchParams.sortBy || "created_at"
+
+  const region = await getRegion(countryCode)
+  if (!region) return null
+
+  const { response } = await getProductsListWithSort({
+    page: 1,
+    queryParams: {
+      limit: 24, // ✅ оптимально для скорости
+      offset: 0,
+    },
+    countryCode,
+    sortBy,
+  })
+
+  const products = response.products as HttpTypes.StoreProduct[]
 
   return (
     <StoreTemplate
+      products={products}
+      region={region}
+      countryCode={countryCode}
       sortBy={sortBy}
-      page={page}
-      categoryId={categoryId}
-      collectionId={collectionId}
-      countryCode={params.countryCode}
     />
   )
 }
