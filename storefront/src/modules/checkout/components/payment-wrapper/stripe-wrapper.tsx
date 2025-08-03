f3c4be52782ba8da123773050e2a82647b91fb87
olegmichailov <olegmikhailov698@gmail.com>
2025-08-03 16:47:37 +0200

    Update stripe-wrapper.tsx

--- storefront/src/modules/checkout/components/payment-wrapper/stripe-wrapper.tsx
+++ storefront/src/modules/checkout/components/payment-wrapper/stripe-wrapper.tsx
@@ -19,9 +19,6 @@ const StripeWrapper: React.FC<StripeWrapperProps> = ({
 }) => {
   const options: StripeElementsOptions = {
     clientSecret: paymentSession!.data?.client_secret as string | undefined,
-    appearance: {
-      theme: "stripe",
-    },
   }
 
   if (!stripeKey) {
