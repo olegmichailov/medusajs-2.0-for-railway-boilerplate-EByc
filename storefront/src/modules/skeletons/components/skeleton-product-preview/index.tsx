4427c4b4a193de31ec08335adab45cf3da7a4d6b
olegmichailov <olegmikhailov698@gmail.com>
2025-08-03 22:55:40 +0200

    Update index.tsx

--- storefront/src/modules/skeletons/components/skeleton-product-preview/index.tsx
+++ storefront/src/modules/skeletons/components/skeleton-product-preview/index.tsx
@@ -1,19 +1,9 @@
-"use client"
 
-import { useEffect, useState } from "react"
 import { Container } from "@medusajs/ui"
 
 const SkeletonProductPreview = () => {
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) return null
-
   return (
-    <div className="w-full animate-pulse">
+    <div className="animate-pulse">
       <Container className="aspect-[9/13.5] w-full bg-gray-100" />
       <div className="flex justify-between text-base-regular mt-2 px-1">
         <div className="w-3/5 h-5 bg-gray-100" />
