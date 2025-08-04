f462cab83d0e1d1d97a2fc84b873c0f0e183a728
olegmichailov <olegmikhailov698@gmail.com>
2025-08-04 19:14:58 +0200

    Update stripe-wrapper.tsx

--- storefront/src/modules/checkout/components/payment-wrapper/stripe-wrapper.tsx
+++ storefront/src/modules/checkout/components/payment-wrapper/stripe-wrapper.tsx
@@ -1,57 +1,23 @@
 "use client"
 
-import { Stripe, StripeElementsOptions } from "@stripe/stripe-js"
-import { Elements } from "@stripe/react-stripe-js"
 import { HttpTypes } from "@medusajs/types"
 import React from "react"
+import PaymentElementForm from "@modules/checkout/components/payment/payment-element-form"
 
 type StripeWrapperProps = {
+  cart: HttpTypes.StoreCart
+  paymentMethods: HttpTypes.StorePaymentCollection[]
   paymentSession: HttpTypes.StorePaymentSession
-  stripeKey?: string
-  stripePromise: Promise<Stripe | null> | null
-  children: React.ReactNode
 }
 
 const StripeWrapper: React.FC<StripeWrapperProps> = ({
-  paymentSession,
-  stripeKey,
-  stripePromise,
-  children,
+  cart,
+  paymentMethods,
 }) => {
-  const clientSecret = paymentSession?.data?.client_secret
-
-  if (!stripeKey) {
-    console.error("❌ Stripe key is missing. Set NEXT_PUBLIC_STRIPE_KEY.")
-    return null
-  }
-
-  if (!stripePromise) {
-    console.error("❌ Stripe promise is missing. Check loadStripe initialization.")
-    return null
-  }
-
-  if (!clientSecret) {
-    console.error("❌ Stripe client secret is missing. Cannot initialize Elements.")
-    return null
-  }
-
-  const options: StripeElementsOptions = {
-    clientSecret,
-    appearance: {
-      theme: "flat",
-      labels: "floating",
-      variables: {
-        fontFamily: "Barlow Condensed, sans-serif",
-        borderRadius: "4px",
-        colorPrimary: "#000000",
-      },
-    },
-  }
-
   return (
-    <Elements options={options} stripe={stripePromise}>
-      {children}
-    </Elements>
+    <div className="flex flex-col gap-y-4">
+      <PaymentElementForm cart={cart} paymentMethods={paymentMethods} />
+    </div>
   )
 }
 
