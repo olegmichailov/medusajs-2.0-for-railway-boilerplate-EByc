7d7b93aca53db398b2754ad788fe0e8030ea5b82
olegmichailov <olegmikhailov698@gmail.com>
2025-08-04 19:19:41 +0200

    Update stripe-wrapper.tsx

--- storefront/src/modules/checkout/components/payment-wrapper/stripe-wrapper.tsx
+++ storefront/src/modules/checkout/components/payment-wrapper/stripe-wrapper.tsx
@@ -1,8 +1,13 @@
 "use client"
 
-import { HttpTypes } from "@medusajs/types"
-import React from "react"
+import React, { useMemo } from "react"
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js"
 import PaymentElementForm from "@modules/checkout/components/payment/payment-element-form"
+import { StripeContext } from "./stripe-context"
+import { HttpTypes } from "@medusajs/types"
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!)
 
 type StripeWrapperProps = {
   cart: HttpTypes.StoreCart
@@ -13,11 +18,31 @@ type StripeWrapperProps = {
 const StripeWrapper: React.FC<StripeWrapperProps> = ({
   cart,
   paymentMethods,
+  paymentSession,
 }) => {
+  const clientSecret = paymentSession?.data?.client_secret
+
+  const options = useMemo(
+    () => ({
+      clientSecret,
+      appearance: {
+        theme: "stripe",
+        labels: "floating",
+      },
+    }),
+    [clientSecret]
+  )
+
+  if (!clientSecret) {
+    return null // или загрузчик
+  }
+
   return (
-    <div className="flex flex-col gap-y-4">
-      <PaymentElementForm cart={cart} paymentMethods={paymentMethods} />
-    </div>
+    <StripeContext.Provider value={true}>
+      <Elements stripe={stripePromise} options={options}>
+        <PaymentElementForm cart={cart} paymentMethods={paymentMethods} />
+      </Elements>
+    </StripeContext.Provider>
   )
 }
 
