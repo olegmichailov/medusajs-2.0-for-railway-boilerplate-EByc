955be032bd58d35aeeb4f8b5a31cca75c7c879b5
olegmichailov <olegmikhailov698@gmail.com>
2025-08-04 22:52:37 +0200

    Update stripe-wrapper.tsx

--- storefront/src/modules/checkout/components/payment-wrapper/stripe-wrapper.tsx
+++ storefront/src/modules/checkout/components/payment-wrapper/stripe-wrapper.tsx
@@ -1,45 +1,42 @@
 "use client"
 
-import React, { createContext, useEffect, useState } from "react"
-import { loadStripe } from "@stripe/stripe-js"
+import { Stripe, StripeElementsOptions } from "@stripe/stripe-js"
 import { Elements } from "@stripe/react-stripe-js"
+import { HttpTypes } from "@medusajs/types"
 
-interface StripeWrapperProps {
+type StripeWrapperProps = {
+  paymentSession: HttpTypes.StorePaymentSession
+  stripeKey?: string
+  stripePromise: Promise<Stripe | null> | null
   children: React.ReactNode
-  clientSecret?: string
 }
 
-export const StripeContext = createContext(false)
+const StripeWrapper: React.FC<StripeWrapperProps> = ({
+  paymentSession,
+  stripeKey,
+  stripePromise,
+  children,
+}) => {
+  const options: StripeElementsOptions = {
+    clientSecret: paymentSession.data?.client_secret,
+  }
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "")
+  if (!stripeKey) {
+    throw new Error("Missing NEXT_PUBLIC_STRIPE_KEY")
+  }
 
-const StripeWrapper: React.FC<StripeWrapperProps> = ({ children, clientSecret }) => {
-  const [stripeReady, setStripeReady] = useState(false)
-  const [options, setOptions] = useState<any>(null)
+  if (!stripePromise) {
+    throw new Error("Stripe not initialized")
+  }
 
-  useEffect(() => {
-    if (!clientSecret) return
-
-    setOptions({
-      clientSecret,
-      appearance: {
-        theme: "flat",
-      },
-    })
-
-    setStripeReady(true)
-  }, [clientSecret])
-
-  if (!clientSecret || !options) {
-    return <>{children}</>
+  if (!paymentSession?.data?.client_secret) {
+    throw new Error("No client_secret provided by Medusa")
   }
 
   return (
-    <StripeContext.Provider value={stripeReady}>
-      <Elements stripe={stripePromise} options={options}>
-        {children}
-      </Elements>
-    </StripeContext.Provider>
+    <Elements options={options} stripe={stripePromise}>
+      {children}
+    </Elements>
   )
 }
 
