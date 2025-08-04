461b4fbefb5677e669b058fc262d911ad89fe207
olegmichailov <olegmikhailov698@gmail.com>
2025-08-04 09:28:22 +0200

    Update index.tsx

--- storefront/src/modules/checkout/templates/checkout-form/index.tsx
+++ storefront/src/modules/checkout/templates/checkout-form/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { listCartShippingMethods } from "@lib/data/fulfillment"
 import { listCartPaymentMethods } from "@lib/data/payment"
 import { HttpTypes } from "@medusajs/types"
@@ -5,6 +7,13 @@ import Addresses from "@modules/checkout/components/addresses"
 import Payment from "@modules/checkout/components/payment"
 import Review from "@modules/checkout/components/review"
 import Shipping from "@modules/checkout/components/shipping"
+import dynamic from "next/dynamic"
+import { loadStripe } from "@stripe/stripe-js"
+
+const StripeWrapper = dynamic(
+  () => import("@modules/checkout/components/payment-wrapper"),
+  { ssr: false }
+)
 
 export default async function CheckoutForm({
   cart,
@@ -24,6 +33,13 @@ export default async function CheckoutForm({
     return null
   }
 
+  const activeStripeSession = cart.payment_collection?.payment_sessions?.find(
+    (s: any) => s.status === "pending" && s.provider_id === "stripe"
+  )
+
+  const stripeKey = process.env.NEXT_PUBLIC_STRIPE_KEY
+  const stripePromise = stripeKey ? loadStripe(stripeKey) : null
+
   return (
     <div>
       <div className="w-full grid grid-cols-1 gap-y-8">
@@ -36,7 +52,17 @@ export default async function CheckoutForm({
         </div>
 
         <div>
-          <Payment cart={cart} availablePaymentMethods={paymentMethods} />
+          {activeStripeSession?.data?.client_secret && stripePromise ? (
+            <StripeWrapper
+              paymentSession={activeStripeSession}
+              stripeKey={stripeKey}
+              stripePromise={stripePromise}
+            >
+              <Payment cart={cart} availablePaymentMethods={paymentMethods} />
+            </StripeWrapper>
+          ) : (
+            <Payment cart={cart} availablePaymentMethods={paymentMethods} />
+          )}
         </div>
 
         <div>
