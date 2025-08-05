ff4b12bde5d5d07a39307ba64952f7c26f812fc3
olegmichailov <olegmikhailov698@gmail.com>
2025-08-05 11:23:53 +0200

    Update index.tsx

--- storefront/src/modules/checkout/components/payment-wrapper/index.tsx
+++ storefront/src/modules/checkout/components/payment-wrapper/index.tsx
@@ -1,36 +1,35 @@
 "use client"
 
-import { loadStripe } from "@stripe/stripe-js"
 import React, { createContext } from "react"
-import StripeWrapper from "./stripe-wrapper"
-import { HttpTypes } from "@medusajs/types"
-import { isStripe } from "@lib/constants"
-
-type WrapperProps = {
-  cart: HttpTypes.StoreCart
-  children: React.ReactNode
-}
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js"
+import { PayPalScriptProvider } from "@paypal/react-paypal-js"
+import { isStripe, isPaypal } from "@lib/constants"
 
 export const StripeContext = createContext(false)
-
 const stripeKey = process.env.NEXT_PUBLIC_STRIPE_KEY
-const stripePromise = stripeKey ? loadStripe(stripeKey) : null
+const stripePromise = loadStripe(stripeKey!)
 
-const Wrapper: React.FC<WrapperProps> = ({ cart, children }) => {
-  const paymentSession = cart.payment_collection?.payment_sessions?.find(
-    (s) => s.status === "pending"
-  )
+const paypalClientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID
 
-  if (isStripe(paymentSession?.provider_id) && stripePromise) {
+const Wrapper = ({ cart, children }: { cart: any; children: React.ReactNode }) => {
+  const paymentSession = cart.payment_collection?.payment_sessions.find((s: any) => s.status === "pending")
+
+  if (isStripe(paymentSession?.provider_id)) {
     return (
       <StripeContext.Provider value={true}>
-        <StripeWrapper
-          paymentSession={paymentSession}
-          stripePromise={stripePromise}
-        >
+        <Elements options={{ clientSecret: paymentSession.data.client_secret }} stripe={stripePromise}>
           {children}
-        </StripeWrapper>
+        </Elements>
       </StripeContext.Provider>
+    )
+  }
+
+  if (isPaypal(paymentSession?.provider_id)) {
+    return (
+      <PayPalScriptProvider options={{ "client-id": paypalClientId, currency: cart.currency_code.toUpperCase(), intent: "authorize" }}>
+        {children}
+      </PayPalScriptProvider>
     )
   }
 
