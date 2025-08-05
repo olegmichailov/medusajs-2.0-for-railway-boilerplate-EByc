0dbbebb7a41d25908e838b127f43e4d9ce417d06
olegmichailov <olegmikhailov698@gmail.com>
2025-08-05 11:26:28 +0200

    Update constants.tsx

--- storefront/src/lib/constants.tsx
+++ storefront/src/lib/constants.tsx
@@ -5,7 +5,6 @@ import Ideal from "@modules/common/icons/ideal"
 import Bancontact from "@modules/common/icons/bancontact"
 import PayPal from "@modules/common/icons/paypal"
 
-/* Map of payment provider_id to their title and icon. Add in any payment providers you want to use. */
 export const paymentInfoMap: Record<
   string,
   { title: string; icon: React.JSX.Element }
@@ -30,21 +29,20 @@ export const paymentInfoMap: Record<
     title: "Manual Payment",
     icon: <CreditCard />,
   },
-  // Add more payment providers here
 }
 
-// This only checks if it is native stripe for card payments, it ignores the other stripe-based providers
 export const isStripe = (providerId?: string) => {
   return providerId?.startsWith("pp_stripe_")
 }
+
 export const isPaypal = (providerId?: string) => {
   return providerId?.startsWith("pp_paypal")
 }
+
 export const isManual = (providerId?: string) => {
   return providerId?.startsWith("pp_system_default")
 }
 
-// Add currencies that don't need to be divided by 100
 export const noDivisionCurrencies = [
   "krw",
   "jpy",
