76a734ae771c52c23529d9122749bf5ba63eefbb
olegmichailov <olegmikhailov698@gmail.com>
2025-08-10 22:18:14 +0200

    Update page.tsx

--- storefront/src/app/[countryCode]/darkroom/page.tsx
+++ storefront/src/app/[countryCode]/darkroom/page.tsx
@@ -1,6 +1,12 @@
-"use client";
-import EditorCanvas from "@/modules/darkroom/EditorCanvas";
+"use client"
+
+import dynamic from "next/dynamic"
+
+const EditorCanvas = dynamic(() => import("@/modules/darkroom/EditorCanvas"), {
+  ssr: false,
+  loading: () => <div className="w-full h-[70vh] flex items-center justify-center">Loading editor…</div>,
+})
 
 export default function DarkroomPage() {
-  return <EditorCanvas />;
+  return <EditorCanvas />
 }
