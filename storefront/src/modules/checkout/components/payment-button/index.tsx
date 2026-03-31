26ffb1d991027dc003e36be0f688592c4043edfc
olegmichailov <olegmikhailov698@gmail.com>
2026-04-01 00:31:25 +0200

    Integrate card element into payment confirmation flow

--- storefront/src/modules/checkout/components/payment-button/index.tsx
+++ storefront/src/modules/checkout/components/payment-button/index.tsx
@@ -79,6 +79,7 @@ const StripePaymentButton = ({
 
   const stripe = useStripe()
   const elements = useElements()
+  const card = elements?.getElement("card")
 
   const session = cart.payment_collection?.payment_sessions?.find(
     (s) => s.status === "pending"
@@ -89,36 +90,32 @@ const StripePaymentButton = ({
   const handlePayment = async () => {
     setSubmitting(true)
 
-    if (!stripe || !elements || !cart) {
+    if (!stripe || !elements || !card || !cart) {
       setSubmitting(false)
       return
     }
 
     await stripe
-      .confirmPayment({
-        elements,
-        confirmParams: {
-          return_url: `${window.location.origin}/order/confirmed`,
-          payment_method_data: {
-            billing_details: {
-              name:
-                cart.billing_address?.first_name +
-                " " +
-                cart.billing_address?.last_name,
-              address: {
-                city: cart.billing_address?.city ?? undefined,
-                country: cart.billing_address?.country_code ?? undefined,
-                line1: cart.billing_address?.address_1 ?? undefined,
-                line2: cart.billing_address?.address_2 ?? undefined,
-                postal_code: cart.billing_address?.postal_code ?? undefined,
-                state: cart.billing_address?.province ?? undefined,
-              },
-              email: cart.email,
-              phone: cart.billing_address?.phone ?? undefined,
+      .confirmCardPayment(session?.data.client_secret as string, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name:
+              cart.billing_address?.first_name +
+              " " +
+              cart.billing_address?.last_name,
+            address: {
+              city: cart.billing_address?.city ?? undefined,
+              country: cart.billing_address?.country_code ?? undefined,
+              line1: cart.billing_address?.address_1 ?? undefined,
+              line2: cart.billing_address?.address_2 ?? undefined,
+              postal_code: cart.billing_address?.postal_code ?? undefined,
+              state: cart.billing_address?.province ?? undefined,
             },
+            email: cart.email,
+            phone: cart.billing_address?.phone ?? undefined,
           },
         },
-        redirect: "if_required",
       })
       .then(({ error, paymentIntent }) => {
         if (error) {
